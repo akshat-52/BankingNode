4a7c90de29b4bcf777f8f221ea79fc700a91ae0c
SaswataBasu <77005822+saswatabasu23@users.noreply.github.com>
2023-11-16 21:13:18 +0530

    -changes in port -react build

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ env.config();
 //MongoDB connection
 //mongodb+srv://root:<password>@cluster0.anmlv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log('DataBase Connected');
+  console.log('DataBase Connected');
 });
 app.use(cors());
 app.use(express.json());
@@ -24,16 +24,16 @@ app.use('/api', adminRoutes);
 
 // Serve the frontend
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/build")));
-    app.get("*", (req, res) =>
-      res.sendFile(
-        path.resolve(__dirname, "../", "frontend", "build", "index.html")
-      )
-    );
-  } else {
-    app.get("/", (req, res) => res.send("Please set to production"));
-  }
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
